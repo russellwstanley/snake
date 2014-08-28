c572f410ddff663af609365df772ad77adc944ff
russell <russellwstanley@gmail.com>
2014-08-28 09:58:35 +0800

    removed old deploy from fabfile

--- deploy/fabfile.py
+++ deploy/fabfile.py
@@ -1,18 +1,6 @@
-from fabric.contrib.project import rsync_project
 from fabric.api import *
 
 dist = "snake-1.0-SNAPSHOT"
-
-env.hosts = ['play-snake.com']
-
-def deploy():
-    local("unzip ../target/universal/"+dist+".zip") 
-    put("upstart/snake.conf", "/etc/init/", use_sudo=True)
-    with settings(warn_only=True):
-        run("sudo stop snake")
-    rsync_project(local_dir=dist+"/", remote_dir="/root/snake", delete=True)
-    run("sudo start snake")
-    local("rm -r "+dist)
 
 def package():
     local("rm -rf "+dist+".zip")
